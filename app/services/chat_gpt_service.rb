b7d3804d6d956e6e89bc204401fc4f2fa3c42458
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-18 12:16:34 +0900

    chatGPTの役目を指定し直し

--- app/services/chat_gpt_service.rb
+++ app/services/chat_gpt_service.rb
@@ -9,7 +9,11 @@ class ChatGptService
     response = @openai.chat(
       parameters: {
         model: "gpt-4o-mini", # Required. # 使用するgpt-4o-miniのエンジンを指定
-        messages: [{ role: "system", content: "You are a helpful assistant." }, { role: "user", content: prompt }],
+        messages: [{ role: "system", 
+                    content: "You are the one who gives us useful knowledge to live by.
+                              Please output knowledge only." },
+                  { role: "user", 
+                    content: prompt }],
         temperature: 0.7, # 応答のランダム性を指定
         max_tokens: 200,  # 応答の長さを指定
       },
