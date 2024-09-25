1a156cf1db7c687b0c1519ea1019a67a9ec27574
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-25 16:47:52 +0900

    DisabledSessionErrorの回避

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
-
+        include DeviseHackFakeSession
         wrap_parameters format: []
 end
